a979bedf80fa7d7f7d2382a7353132704568ce31
ashther <ashther@qq.com>
2015-09-07 16:08:48 +0800

    修改co_occurrence为评分加权

--- item_based.py
+++ item_based.py
@@ -25,14 +25,29 @@ def coocCreateIB(df):
         rated = df[df['user'] == u]['item']
         
         for ra in rated:
-            co_occurrence.loc[ra, ra] += 1
+            co_occurrence.loc[ra, ra] += \
+                df.ix[(df['user']==u) & (df['item']==ra), 'val'].tolist()[0] ** 2
         
         if len(rated) > 1:
             for per in list(permutations(rated, 2)):
-                co_occurrence.loc[per[0], per[1]] += 1
+                co_occurrence.loc[per[0], per[1]] += \
+                df.ix[(df['user']==u) & (df['item']==per[0]), 'val'].tolist()[0] * \
+                df.ix[(df['user']==u) & (df['item']==per[1]), 'val'].tolist()[0]
     
     return co_occurrence
+
+def raMatCreate(co_occurrence, u, df):
+    res = [0 for _ in range(len(co_occurrence))]
+    for i, ind in enumerate(co_occurrence.index):
+        try:
+            res[i] = df.ix[(df['user']==u) & (df['item']==ind), 'val'].tolist()[0]
+        except:
+            continue
+    return np.array(res)
+
+
     
 if __name__ == '__main__':
     co_occurrence = coocCreateIB(df)
-    print co_occurrence
+    raMat = raMatCreate(co_occurrence, 1, df)
+    print np.dot(co_occurrence, raMat)
