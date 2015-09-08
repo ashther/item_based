ddd6e68fb8510aecd5f4348537959fdcda145010
ashther <ashther@qq.com>
2015-09-08 14:35:45 +0800

    增加了利用dict实现协作过滤的版本
    item_based_dict实现了利用dict数据结构完成协作过滤的同现矩阵创建、评分向量创建、过滤的全过程

--- dataSource.py
+++ dataSource.py
@@ -4,9 +4,20 @@ Created on Mon Sep 07 09:13:49 2015
 
 @author: Administrator
 """
+import csv
+import json
+from collections import defaultdict
+#import pandas as pd
+#
+#df = pd.read_csv('rating.csv')
+#df = df.rename(columns = {'user_id':'user', 'group_id':'item'})
 
-import pandas as pd
-import numpy as np
+rating = defaultdict(dict)
 
-df = pd.read_csv('rating.csv')
-df = df.rename(columns = {'user_id':'user', 'group_id':'item'})
+def VPrint(content):
+    print json.dumps(content, encoding='utf-8', ensure_ascii=False, indent=1)
+
+f = open('rating.csv', 'rb')
+for line in csv.reader(f):
+    rating[line[0]][line[1]] = float(line[2])
+f.close()
---
+++ item_based_dict.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 08 10:07:00 2015
+
+@author: Administrator
+"""
+import pandas as pd
+import numpy as np
+from itertools import permutations
+from dataSource import *
+
+def coocCreate(rating):
+    #users = sorted(rating.keys())
+    items = sorted(list(set([y for x in rating.values() for y in x.keys()])))
+    
+    co_occurrence = pd.DataFrame(0, index=items, columns=items)
+    
+    for u, ir in rating.items():
+        for i in ir.keys():
+            co_occurrence.loc[i, i] += ir[i] * ir[i]
+        if len(ir.keys()) > 1:
+            for per in list(permutations(ir.keys(), 2)):
+                co_occurrence.loc[per[0], per[1]] += ir[per[0]] * ir[per[1]]
+    return co_occurrence
+
+def raMatCreate(co_occurrence, u, rating):
+    res = np.zeros(len(co_occurrence))
+    for i, ind in enumerate(co_occurrence.index):
+        if ind in rating[str(u)].keys():
+            res[i] = rating[str(u)][ind]
+    return res
+    
+def recommend(u, rating):    
+    co_occurrence = coocCreate(rating)
+    rating_now = raMatCreate(co_occurrence, u, rating)
+    rating_predict = np.dot(co_occurrence, rating_now)
+    recommend = dict(zip(co_occurrence.index, rating_predict))
+    
+    return sorted([(k, v) for k, v in recommend.items() \
+                    if k not in rating[str(u)].keys()], 
+                   key=lambda recmd:recmd[1], reverse=True)
+
+if __name__ == '__main__':
+    print recommend(5, rating)
